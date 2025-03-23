74ea464a7a96bace0355b053f9b365f6ebeecda5
Jamon-exe <103723438+Jamon-exe@users.noreply.github.com>
2025-03-23 13:11:43 +0000

    def syntax used properly

--- CLASS ACTIVITIES/test.py
+++ CLASS ACTIVITIES/test.py
@@ -1,10 +1,14 @@
-def new_func (holder,i):
-    holder = 0
-    i = 1
-    while i > 0:
-        i = int(input("Number"))
-        if (i % 2 == 0) and (i % 4 != 0):
-            holder += i
-    print (holder)
+def sumo(num1,num2):
+    num1 = 1
+    num2 = 2
+    sumno = num1 + num2
+    print(sumno)
 
-new_func(9,1)
+sumo(1,2)
+# holder = 0
+# i = 1
+# while i > 0:
+#     i = int(input("Number"))
+#     if (i % 2 == 0) and (i % 4 != 0):
+#         holder += i
+# print (holder)
