e23a4f8bcf368cba985bf696fbfcfbe96cff1e85
Jamon-exe <103723438+Jamon-exe@users.noreply.github.com>
2025-03-26 14:22:39 +0000

    updated assignment

--- ASSIGNMENTS/Assignment7.py
+++ ASSIGNMENTS/Assignment7.py
@@ -2,3 +2,5 @@
 # Find out why a[-1:-4] returns an empty list
 
 #Assignmet 2
+# find other functions that can be used on a list. 
+# Those that return a new list and those that do not return a new list. Categorize them.
