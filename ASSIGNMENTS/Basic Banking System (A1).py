1b98bc5e715c88414c8f2b1d0d46a08a0112705c
Jamon King <103723438+Jamon-exe@users.noreply.github.com>
2025-04-02 15:00:26 +0000

    added transaction recording, fixed a few bugs, addef transaction option to the menu, updated security on pin changing: requiring the user to input current code first.

--- ASSIGNMENTS/Basic Banking System (A1).py
+++ ASSIGNMENTS/Basic Banking System (A1).py
@@ -1,18 +1,24 @@
+import datetime
+
 pin="0"
+new_pin="1234"
 balance= 0
 attempts=3
+transactions= []
+now = datetime.datetime.now()
+print (f"Welcome to the Bank of Python. Today's date is {now.strftime('%Y-%m-%d %H:%M:%S')}")
 
-while pin != "1234":
+while pin != new_pin:
     pin=input("Enter Pin: ")
     
-    if pin == "1234":
+    if pin == new_pin:
         print ("PIN correct\n")
 
         condition = "N"
         while condition == "N":
             choice= 0
-            while choice not in ["C", "D", "W", "E", "Z"]:
-                choice = input("Menu:\nC. Check Balance\nD. Deposit Money\nW. Withdraw Money\nE. Exit Progam\nZ. Change PIN\n\n").upper()
+            while choice not in ["C", "D", "W", "E", "Z", "T"]:
+                choice = input("Menu:\nC. Check Balance\nD. Deposit Money\nW. Withdraw Money\nE. Exit Progam\nZ. Change PIN\nT. Check Transaction History \n\n").upper()
 
                 if choice == "C":
                     print(f"Your balance {balance}")
@@ -25,6 +31,8 @@ while pin != "1234":
                     else:
                         balance += deposit
                         print(f"Deposit successful. New balance: {balance}")
+                        d= f"Deposit of {deposit} at {now.strftime('%Y-%m-%d %H:%M:%S')}"
+                        transactions.append(d)
 
 
                 elif choice == "W":
@@ -46,9 +54,19 @@ while pin != "1234":
                         elif balance > withdraw:
                             balance -= withdraw
                             print(f"Withdrawal successful. Your balance is: {balance}")
+                            w = f"Withdrawal of {withdraw} at {now.strftime('%Y-%m-%d %H:%M:%S')}"
+                            transactions.append(w)
                             break
                         
                 elif choice == "Z":
+
+                    while True:
+                        prev_pin=input('Enter current pin: ')
+                        if prev_pin == pin:
+                            print("Pin correct")
+                            break
+                        else:
+                            print("Incorrect pin")
                     new_pin = input("Enter new pin: ")
                     if new_pin == pin:
                         print("New pin cannot be the same as the old pin")
@@ -58,9 +76,17 @@ while pin != "1234":
                         pin = new_pin
                         print("Pin changed successfully")
                 
+                
                 elif choice == "E":
                     print("Exiting Program. Goodbye!")
                     break
+
+                elif choice == "T":
+                    for a in reversed(transactions):
+                        print(a)
+                    if len(transactions) == 0:
+                        print("No transactions yet\n")
+                    
 
                 else:
                     print("Incorrect Input.Try Again")
